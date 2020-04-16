edcac929b2318d9df4d0dd406fcf3ce10bcf9b08
codexxx17 <56841635+codexxx17@users.noreply.github.com>
2020-04-16 18:48:46 +0300

    34 complete lesson, Added all need files Отношения Eloqment:Relationships первые шаги

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -3,15 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
     use SoftDeletes;
 
-    /*protected $dates
-        = [
-            'published_at',
-        ];*/
+    /**Категория статьи
+     * @return BelongsTo
+     */
+    public function category()
+    {
+        //Статья принадлежит категории
+        return $this->belongsTo(BlogCategory::class);
+    }
 
+    /** Автор статьи
+     * @return BelongsTo
+     */
+    public function user()
+    {
+        //Статья принадлежит пользователю
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ app/Repositories/BlogPostRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogPost as Model;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+/**
+ * Class BlogPostRepository
+ * @package App\Repositories
+ */
+class BlogPostRepository extends CoreRepository
+{
+    /**
+     * @return mixed|string
+     */
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+    public function getAllWithPaginate()
+    {
+        $columns = [
+            'id',
+            'title',
+            'slug',
+            'is_published',
+            'published_at',
+            'user_id',
+            'category_id',
+        ];
+
+        return $this->startConditions()
+                    ->select($columns)
+                    ->orderBy('id', 'DESC')
+                    ->with([
+                        //можно так
+                        'category' => function ($query){
+                            $query->select(['id', 'title']);
+                        },
+                        // или так
+                        'user:id,name',
+                        ])
+                    ->paginate(25);
+    }
+}
+
